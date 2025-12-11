f5c24cc6605aaf24cb0e69137eeec88a79fddacd
DARKPINGGD <omerasarfino@gmail.com>
2025-12-11 21:52:50 +0300

    Create about.js

---
+++ Pages/about.js
@@ -0,0 +1,12 @@
+import Nav from '../components/Nav'
+export default function About(){
+return (
+<div className="min-h-screen">
+<Nav />
+<main className="p-8">
+<h1 className='text-2xl font-bold'>About</h1>
+<p className='mt-2'>This project is maintained by the community.</p>
+</main>
+</div>
+)
+}
